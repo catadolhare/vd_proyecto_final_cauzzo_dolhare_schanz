190a53fd2e6c2996e8bbc9bdcbc48b1b6e8ee770
CatalinaDolhare <catalinadolhare@gmail.com>
2024-06-05 11:22:28 -0300

    graficos y portada

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,30 @@
 <!-- Script JS -->
 <script>
-</script>
 
-<!-- Estructura contenido HTML -->
-<h1>Representación del consumo de agua en las estaciones</h1>
-<div class='container'>
-    <div class='item'>
-        <img src="./images/dimension25.jpeg" alt="Consumo de agua en invierno (25)">
-        <h6>Consumo de agua en verano</h6>
-        <p>Magnitud 25</p>
-    </div>
-    <div class='item'>
-        <img src="./images/dimension100.jpeg" alt="Consumo de agua en verano (100)">
-        <h6>Consumo de agua en verano</h6>
-        <p>Magnitud 100</p>
-    </div>
+</script>
+<div class="portada">
+    <img src="images/portada.png" alt="Portada">
 </div>
+<div>
+    <iframe title="Mundial 2022" aria-label="Mapa" id="datawrapper-chart-4ppPT" src="https://datawrapper.dwcdn.net/4ppPT/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="294" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();</script>
+    <!--ACLRAR:GALES EN 2022-->
+</div>
+<div>
+    <iframe title="Mundial 1986" aria-label="Map" id="datawrapper-chart-BWQLZ" src="https://datawrapper.dwcdn.net/BWQLZ/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="294" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();
+    </script>
+    <!--ACLRAR: WEST GERMANY/SCOTLAND, NORTH IRELAND EN 1986-->
+</div>
+<div>
+    <iframe title="Mundial 1978" aria-label="Map" id="datawrapper-chart-oL8XV" src="https://datawrapper.dwcdn.net/oL8XV/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="294" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();
+    </script>
+    <!--ACLRAR: WEST GERMANY/SCOTLAND 18986 Y 1978-->
+</div>
+
+
+
 
 
 <!-- Estilos CSS -->
 <style>
-    img{
-        width: 250px;
-        height: 250px;
-        text-align: center;
-    }
-    h1{
-        font-family: 'Times New Roman', Times, serif;
-        font-weight: bold;
-        text-align: center;
-        text-decoration: underline;
-    }
-    h6{
-        font-size: 20px;
-        text-align: center;
-    }
-    p{
-        font-size: 15px;
-        text-align: center;
-    }
-    .item{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    .container{
-        display: flex;
-        justify-content: space-evenly;
-    }
+   
 </style>
