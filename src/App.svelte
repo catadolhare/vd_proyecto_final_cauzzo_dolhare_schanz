7b55fdafbcb9de78879bdae6456562de6ddae501
CatalinaDolhare <catalinadolhare@gmail.com>
2024-06-12 09:13:49 -0300

    graficos y explicaciones

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
     </head>
     <div class="intro">
         <div class="portada">
@@ -41,33 +41,36 @@
     </div>
     <div class="paises">
         <div class="grafico_paises">
-            <iframe title="Mundial 2022" aria-label="Mapa" id="datawrapper-chart-4ppPT" src="https://datawrapper.dwcdn.net/4ppPT/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="294" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();</script>
-            <p class='aclaracion'>*Gales forma parte del Reino Unido con el puesto numero 30</p>
-        </div>
-        <div class="texto-paises">
-            <p>hola</p>
-        </div>
-    </div>
-    <div class="paises">
-        <div class="grafico_paises">
-            <iframe title="Mundial 1986" aria-label="Map" id="datawrapper-chart-BWQLZ" src="https://datawrapper.dwcdn.net/BWQLZ/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="294" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();</script>
-            <p class='aclaracion'>*Alemania Occidental puesto numero 2</p>
-            <p class='aclaracion'>**Escocia forma parte del Reino Unido con puesto 19</p>
-            <p class='aclaracion'>***Irlanda del norte con puesto 21</p>
-        </div>
-        <div class="texto-paises">
-            <p>hola</p>
-        </div>
-    </div>
-    <div class="paises">
-        <div class="grafico_paises">
             <iframe title="Mundial 1978" aria-label="Map" id="datawrapper-chart-oL8XV" src="https://datawrapper.dwcdn.net/oL8XV/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="294" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();</script>
 
             <p class='aclaracion'>*Alemania Occidental puesto numero 6</p>
             <p class="aclaracion">**Escocia forma parte del Reino Unido con puesto 11</p>
         </div>
         <div class="texto-paises">
-            <p>hola</p>
+            <h3>1978</h3>
+            <p>, contó con la participación de 16 países. Este torneo fue notable por la gran competencia y el ambiente festivo que se vivió. Argentina se coronó campeona del mundo por primera vez en su historia, tras vencer a Países Bajos en el Monumental.</p>
+        </div>
+    </div>
+    <div class="paises">
+        <div class="texto-paises">
+            <h3>1986</h3>
+            <p>, celebrado en México, reunió a 24 equipos de diversas partes del mundo. Este torneo es recordado por las actuaciones estelares de Diego Maradona, incluyendo su famoso "Gol del Siglo" y la "Mano de Dios". Argentina venció a Alemania y se consgró campeona nuevamente. </p>
+        </div>
+        <div class="grafico_paises">
+            <iframe title="Mundial 1986" aria-label="Map" id="datawrapper-chart-BWQLZ" src="https://datawrapper.dwcdn.net/BWQLZ/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="294" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();</script>
+            <p class='aclaracion'>*Alemania Occidental puesto numero 2</p>
+            <p class='aclaracion'>**Escocia forma parte del Reino Unido con puesto 19</p>
+            <p class='aclaracion'>***Irlanda del norte con puesto 21</p>
+        </div>
+    </div>
+    <div class="paises">
+        <div class="grafico_paises">
+            <iframe title="Mundial 2022" aria-label="Mapa" id="datawrapper-chart-4ppPT" src="https://datawrapper.dwcdn.net/4ppPT/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="294" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();</script>
+            <p class='aclaracion'>*Gales forma parte del Reino Unido con el puesto numero 30</p>
+        </div>
+        <div class="texto-paises">
+            <h3>2022</h3>
+            <p>, organizado en Qatar, el primer torneo mundialista realizado en el Medio Oriente y durante los meses de noviembre y diciembre, con 32 selecciones, notable por sus innovaciones tecnológicas y su enfoque en la sostenibilidad. Argentina se coronó campeona por tercera vez, venciendo a Francia en una final épica.</p>
         </div>
     </div> 
 </main>
@@ -90,11 +93,21 @@
         color: #2A1552;
     }
     h2{
-        font-family: "Noto Sans KR", sans-serif;
+        font-family: "Fredoka", sans-serif;
         font-optical-sizing: auto;
-        font-weight: 900;
+        font-weight: 700;
         font-style: normal;
+        font-variation-settings:
+            "wdth" 100;
         text-transform: uppercase;
+    }
+    h3{
+        font-family: "Fredoka", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 700;
+        font-style: normal;
+        font-variation-settings:
+            "wdth" 100;
     }
     .intro{
         display: flex;
@@ -140,21 +153,33 @@
         align-items: flex-start;
         flex-direction: row;
         justify-content: center;
-        background-color: aliceblue;
+        margin: 10px 100px;
+        padding: 20px 0;
     }
     .grafico_paises{
-        padding: 0 10px;
+        padding: 0 20px;
         width: 50%;
         display: flex; /* Para centrar la imagen dentro de la columna */
         flex-direction: column;
         background-image: url(images/fondo_paises.png); /* Solo para visualización */
+        margin: 0 10px;
     }
     .grafico_paises iframe{
         width: 85%;
         height: auto;
+        margin-top: 40px;
     }
     .texto-paises{
         width: 50%;
+        margin: 0 10px;
+    }
+    .texto-paises h3{
+        display: inline;
+        color: #FAFF00;
+        font-size: 50px;
+    }
+    .texto-paises p{
+        display: inline;
     }
     .aclaracion{
         font-style: italic;
